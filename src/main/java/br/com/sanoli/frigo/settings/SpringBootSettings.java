9cadf985605dfd207ed9a4fc11a43b775b5007ac
danilo.sanoli <danilo.sanoli@gmail.com>
2017-06-24 01:41:20 -0300

    Container do Spring boot e dependências dos starters

---
+++ src/main/java/br/com/sanoli/frigo/settings/SpringBootSettings.java
@@ -0,0 +1,14 @@
+package br.com.sanoli.frigo.settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootSettings {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSettings.class, args);
+	}
+
+}
