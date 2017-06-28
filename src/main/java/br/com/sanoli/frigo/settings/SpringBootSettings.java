b2415c586b3ac89e616a46d60b12c123f6c74eea
Danilo Oliveira <danilo.sanoli@gmail.com>
2017-06-27 21:59:20 -0300

    Criando conexão com o Banco de Dados

--- src/main/java/br/com/sanoli/frigo/settings/SpringBootSettings.java
+++ src/main/java/br/com/sanoli/frigo/settings/SpringBootSettings.java
@@ -1,7 +1,11 @@
 package br.com.sanoli.frigo.settings;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 
 @SpringBootApplication
@@ -9,6 +13,16 @@ public class SpringBootSettings {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSettings.class, args);
+	}
+	
+	@Bean
+	public DataSource createDataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl(""); // Criar banco de dados local para testes
+		dataSource.setUsername(""); // Criar usuario para testes
+		dataSource.setPassword(""); // Criar senha para usuário
+		return dataSource;
 	}
 
 }
